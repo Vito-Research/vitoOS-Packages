0d4dd1697cea0fa71c5618ce9d2904a1a1ec665c
senalbulumulle <senal.bulumulle@gmail.com>
2022-08-28 15:07:58 -0400

    Added script: [python-streamlit/dependencies.sh]

---
+++ python-streamlit/dependencies.sh
@@ -0,0 +1,19 @@
+ 
+sudo pacman -S python-altair
+sudo pacman -S python-astor
+sudo pacman -S python-base58
+sudo pacman -S python-blinker
+sudo pacman -S python-cachetools
+sudo pacman -S python-dateutil
+sudo pacman -S python-gitpython
+sudo pacman -S python-pandas
+sudo pacman -S python-protobuf
+sudo pacman -S python-pyarrow
+sudo pacman -S python-pydeck
+sudo pacman -S python-pympler
+sudo pacman -S python-toml
+sudo pacman -S python-tornado
+sudo pacman -S python-tzlocal
+sudo pacman -S python-validators
+sudo pacman -S python-watchdog
+
