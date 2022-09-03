f236320c9813908e61307fd6f227b8b3c810e422
senalbulumulle <senal.bulumulle@gmail.com>
2022-09-03 15:29:04 -0400

    Updated python-streamlit/dependencies.sh

--- python-streamlit/dependencies.sh
+++ python-streamlit/dependencies.sh
@@ -1,25 +1,25 @@
  
-sudo pacman -S python-altair
-sudo pacman -S python-astor
-sudo pacman -S python-base58
-sudo pacman -S python-blinker
-sudo pacman -S python-cachetools
-sudo pacman -S python-dateutil
-sudo pacman -S python-gitpython
-sudo pacman -S python-pandas
-sudo pacman -S python-protobuf
-sudo pacman -S python-pyarrow
-sudo pacman -S python-pydeck
-sudo pacman -S python-pympler
-sudo pacman -S python-toml
-sudo pacman -S python-tornado
-sudo pacman -S python-tzlocal
-sudo pacman -S python-validators
-sudo pacman -S python-watchdog
+sudo pacman -S --noconfirm python-altair
+sudo pacman -S --noconfirm python-astor
+sudo pacman -S --noconfirm python-base58
+sudo pacman -S --noconfirm python-blinker
+sudo pacman -S --noconfirm python-cachetools
+sudo pacman -S --noconfirm python-dateutil
+sudo pacman -S --noconfirm python-gitpython
+sudo pacman -S --noconfirm python-pandas
+sudo pacman -S --noconfirm python-protobuf
+sudo pacman -S --noconfirm python-pyarrow
+sudo pacman -S --noconfirm python-pydeck
+sudo pacman -S --noconfirm python-pympler
+sudo pacman -S --noconfirm python-toml
+sudo pacman -S --noconfirm python-tornado
+sudo pacman -S --noconfirm python-tzlocal
+sudo pacman -S --noconfirm python-validators
+sudo pacman -S --noconfirm python-watchdog
 
-yay -S python-base58
-yay -S python-click
-yay -S python-pydeck
-yay -S python-pympler
-yay -S python-requests
-yay -S python-watchdog
+yay -S --noconfirm python-base58
+yay -S --noconfirm python-click
+yay -S --noconfirm python-pydeck
+yay -S --noconfirm python-pympler
+yay -S --noconfirm python-requests
+yay -S --noconfirm python-watchdog
